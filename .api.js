5bd1a438a1f3d86f93accc2960deae7a0f878a44
W. Dev Endd <111259650+Wd-Endd@users.noreply.github.com>
2025-01-24 15:41:41 +0000

    ;-;

--- .api.js
+++ .api.js
@@ -1,5 +1,6 @@
 export const beautifulPic = [
     {
-        img: ""
-    }
+        img: "https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://gcs.tripi.vn/public-tripi/tripi-feed/img/474344GxR/anh-4k-chill_040215716_thumb.jpg",
+        title: "chill wallpaper",
+    },
 ];
--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
+import { } from "../.api.js";
 import Thumnail from "./component/Thumbnail";
 
 function App() {
   return (
     <div class="content-grid">
-      <Thumnail
-        img="https://media.discordapp.net/attachments/1316994819222011926/1331622638891831348/1737541879417.png?ex=6794ecb9&is=67939b39&hm=27134b59444a53d38f30ddac122421e99390d4625cae715f6636c2621156cd5b&"
-        title="Alime :9"
-      />
-      <Thumnail
-        img="https://media.discordapp.net/attachments/1316994819222011926/1331622638891831348/1737541879417.png?ex=6794ecb9&is=67939b39&hm=27134b59444a53d38f30ddac122421e99390d4625cae715f6636c2621156cd5b&"
-        title="Alime :9"
-      />
-      <Thumnail
-        img="https://media.discordapp.net/attachments/1316994819222011926/1331622638891831348/1737541879417.png?ex=6794ecb9&is=67939b39&hm=27134b59444a53d38f30ddac122421e99390d4625cae715f6636c2621156cd5b&"
-        title="Alime :9"
-      />
+
     </div>
   );
 }

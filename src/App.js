6b53d151439482b9e320e6fbf508ec769a16b3b1
W. Dev Endd <111259650+Wd-Endd@users.noreply.github.com>
2025-01-25 08:27:30 +0000

    ;-;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { beautifulThumb } from "./.api";
+import CaroBoard from "./component/CaroBoard";
 import Toggle from "./component/state/Toggle";
 import Thumnail from "./component/Thumbnail";
 
 function App() {
   return (
     <div>
+      <CaroBoard />
+
+      <div style={{ height: "200px"}}></div>
 
       <Toggle />
 
---
+++ src/component/CaroBoard.jsx
@@ -0,0 +1,20 @@
+import "./CaroBoard.css";
+
+function CaroBoard() {
+    return (
+        <div className="caro-board">
+            Hello
+            fddd
+            4g3g
+            4gtg3
+            gtgtgg4
+            g4g
+            4g3g
+            3g
+            g3
+            f
+        </div>
+    )
+}
+
+export default CaroBoard

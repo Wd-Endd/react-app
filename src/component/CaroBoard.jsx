b228618a38a0f1a6f2fae564b272a00e880ccaad
W. Dev Endd <111259650+Wd-Endd@users.noreply.github.com>
2025-01-25 12:09:55 +0000

    ;-;

--- src/component/CaroBoard.jsx
+++ src/component/CaroBoard.jsx
@@ -3,16 +3,15 @@ import "./CaroBoard.css";
 function CaroBoard() {
     return (
         <div className="caro-board">
-            Hello
-            fddd
-            4g3g
-            4gtg3
-            gtgtgg4
-            g4g
-            4g3g
-            3g
-            g3
-            f
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
+            <cell></cell>
         </div>
     )
 }

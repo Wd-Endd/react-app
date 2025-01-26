fcf29d02b90d21adef07a29b0b119193f65daf9d
W. Dev Endd <111259650+Wd-Endd@users.noreply.github.com>
2025-01-26 06:27:41 +0000

    ;-;

--- src/tictactoe .js
+++ src/tictactoe .js
@@ -6,15 +6,23 @@ export const turner = {
     }
 }
 
-export const winConditions = [
-    [ 0, 1, 2 ],
-    [ 3, 4, 5 ],
-    [ 6, 7, 8 ],
+export function winCalc(cells) {
+    const winConditions = [
+       [ 0, 1, 2 ],
+       [ 3, 4, 5 ],
+       [ 6, 7, 8 ],
+   
+       [ 0, 3, 6 ],
+       [ 1, 4, 7 ],
+       [ 2, 5, 8 ],
+   
+       [ 0, 4, 8 ],
+       [ 2, 4, 6 ],
+   ];
 
-    [ 0, 3, 6 ],
-    [ 1, 4, 7 ],
-    [ 2, 5, 8 ],
-
-    [ 0, 4, 8 ],
-    [ 2, 4, 6 ],
-]
+   winConditions.forEach(([a, b, c]) => {
+    if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        return cells[a];
+    }
+   })
+}

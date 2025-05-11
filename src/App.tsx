98b0b993236743517d6dc28a9a8a23ca80f65665
Wd-Endd <endercitihacker11111@gmail.com>
2025-05-11 09:47:38 +0700

    ;-;

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,13 @@
-import Header from './components/Header';
-import CardGallery from './components/Card';
-import React, { createContext, useReducer, useEffect, useState } from 'react';
-import "./App.scss";
-import list from "./API";
-import ViewModeTransform from "./events/ViewModeTransform";
-import eruda from "eruda";
-// import refStore from "./refStore";
-
-
-const AppContext = createContext(Object.create({}));
-
-export interface indexAction {
-  type: string;
-  payload?: number;
-}
-
-function indexReducer(state: number, action: indexAction) {
-  switch (action.type) {
-      case "NEXT":
-        return state + 1;
-      case "PREV":
-        return state - 1;
-      case "SELECT":
-        return typeof action.payload === "number" ? action.payload : state;
-    default:
-      return state;
-  }
-}
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [ selectedIndex, disPatch ] = useReducer(indexReducer, 0);
-  const [ viewMode, setViewMode ] = useState(true);
-  useEffect(() => {
-    document.body.style.backgroundImage = `url(${list[selectedIndex].bg})`
-  }, [selectedIndex]);
-  useEffect(() => {
-    if (!viewMode) {
-      ViewModeTransform.start();
-    } else {
-      ViewModeTransform.destroy();
-    }
-  }, [viewMode]);
-  useEffect(() => {
-    if (process.env.NODE_ENV === "development") { eruda.init(); }
-  }, []);
-
   return (
-    <AppContext.Provider value={{
-      selectedIndex,
-      disPatch,
-      viewMode,
-      setViewMode,
-    }} 
-    >
-      <Header />
-      <div className="blur-bg"></div>
-      <div className="content-bg h-screen p-10">
-        <CardGallery />
-      </div>
-    </AppContext.Provider>
+    <div className="App">
+      <p>Hello World</p>
+    </div>
   );
 }
 
 export default App;
-export { AppContext };
--- src/refStore.ts
+++
@@ -1,7 +0,0 @@
-import { createRef } from "react";
-
-const refStore = {
-  flexLessElement: createRef<HTMLDivElement>(),
-};
-
-export default refStore
